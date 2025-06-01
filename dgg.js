ecb5d4d15453be029c3843ce95f022a63ddf8a27
yash <yashraw111@gmail.com>
2025-06-01 08:03:45 +0530

    website done

---
+++ dgg.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+  });
